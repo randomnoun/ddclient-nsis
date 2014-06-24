5abd0fdf37b1bfd60950f40de17e8a14fd819e92
knoxg <>
2014-06-24 21:19:34 +0000

    --icon not supported in latest pp; added Win32::Exe workaround

--- src/main/perl/make-ddclient-exe.bat
+++ src/main/perl/make-ddclient-exe.bat
@@ -6,8 +6,16 @@ rem NB(1): supplying an .ico containing anything over 32x32 here doesn't work
 rem NB(2): calling  c:\perl\site\bin\pp.bat here doesn't work
 rem NB(3): this doesn't seem to work in 64-bit perl, either
 
-%PERL% %PERL%\..\..\site\bin\pp --icon "..\resources\ddclient32.ico" -o ..\..\..\target\ddclient.exe ddclient
-%PERL% %PERL%\..\..\site\bin\pp --gui --icon "..\resources\ddclient32.ico" -o ..\..\..\target\ddclient-noconsole.exe ddclient
+rem --icon no longer supported in pp (see http://stackoverflow.com/questions/22607334 )
+rem %PERL% %PERL%\..\..\site\bin\pp --icon "..\resources\ddclient32.ico" -o ..\..\..\target\ddclient.exe ddclient
+rem %PERL% %PERL%\..\..\site\bin\pp --gui --icon "..\resources\ddclient32.ico" -o ..\..\..\target\ddclient-noconsole.exe ddclient
+
+%PERL% %PERL%\..\..\site\bin\pp -o ..\..\..\target\ddclient.exe ddclient
+%PERL% -e "use Win32::Exe; $exe = Win32::Exe->new('..\..\..\target\ddclient.exe'); $exe->set_single_group_icon('..\resources\ddclient32.ico'); $exe->write;"
+
+%PERL% %PERL%\..\..\site\bin\pp --gui -o ..\..\..\target\ddclient-noconsole.exe ddclient
+%PERL% -e "use Win32::Exe; $exe = Win32::Exe->new('..\..\..\target\ddclient-noconsole.exe'); $exe->set_single_group_icon('..\resources\ddclient32.ico'); $exe->write;"
+
 
 rem verpath sourced from http://www.codeproject.com/Articles/37133/Simple-Version-Resource-Tool-for-Windows
 rem NB(3): verpatch appears to trim the last character from the strings here in procexp (but not in windows explorer)
