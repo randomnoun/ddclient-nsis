2778a559e78beb56303e18e8032d37c3eec340d7
knoxg <knoxg@yttrium.mshome.net>
2021-07-25 16:53:48 +1000

    windows 10 build: 64-bit strawberry perl, use resourcehacker to substitute icons

--- src/main/perl/make-ddclient-exe.bat
+++ src/main/perl/make-ddclient-exe.bat
@@ -1,6 +1,5 @@
 @echo off
 rem make-ddclient-exe.bat
-rem $Id$
 rem
 rem Batch file to compile the ddclient perl source into a gui and console .exe files, then post-process
 rem these files to add Windows icon and version resources.
@@ -8,6 +7,7 @@ rem these files to add Windows icon and version resources.
 
 rem parameters are set from within mvn
 set DDCLIENT_VERSION=%1
+echo Building ddclient %DDCLIENT_VERSION%
 
 rem see http://search.cpan.org/~rschupp/PAR-Packer-1.014/lib/pp.pm
 rem NB(1): supplying an .ico containing anything over 32x32 here doesn't work
@@ -15,20 +15,30 @@ rem NB(2): calling  c:\perl\site\bin\pp.bat here doesn't work
 rem NB(3): this doesn't seem to work in 64-bit perl, either
 rem NB(4): verpatch appears to trim the last character from the strings here in procexp (but not in windows explorer)
 
-rem verpath sourced from http://www.codeproject.com/Articles/37133/Simple-Version-Resource-Tool-for-Windows
+rem verpatch sourced from http://www.codeproject.com/Articles/37133/Simple-Version-Resource-Tool-for-Windows
+rem rcedit sourced from https://github.com/electron/rcedit/releases/tag/v1.1.1
 
 rem --icon no longer supported in pp (see http://stackoverflow.com/questions/22607334 )
-rem %PERL% %PERL%\..\..\site\bin\pp --icon "..\resources\ddclient32.ico" -o ..\..\..\target\ddclient.exe ddclient
-rem %PERL% %PERL%\..\..\site\bin\pp --gui --icon "..\resources\ddclient32.ico" -o ..\..\..\target\ddclient-noconsole.exe ddclient
+rem %PERL% %PERL%\..\..\site\bin\pp --icon "..\resources\ddclient32.ico" -o ..\..\..\target\ddclient-noicon.exe ddclient
+rem %PERL% %PERL%\..\..\site\bin\pp --gui --icon "..\resources\ddclient32.ico" -o ..\..\..\target\ddclient-noconsole-noicon.exe ddclient
 
 echo Compiling ddclient.exe
-%PERL% %PERL%\..\..\site\bin\pp -o ..\..\..\target\ddclient.exe ddclient
-%PERL% -e "use Win32::Exe; $exe = Win32::Exe->new('..\..\..\target\ddclient.exe'); $exe->set_single_group_icon('..\resources\ddclient32.ico'); $exe->write;"
-verpatch ..\..\..\target\ddclient.exe %DDCLIENT_VERSION% /fn /s company "randomnoun" /s description "A Dynamic DNS client" /s copyright "(c) 2013-2014 randomnoun Creative Commons Attribution 3.0 Unported License" /s product ddclient /pv %DDCLIENT_VERSION%
+%PERL% %PERL%\..\..\site\bin\pp -o ..\..\..\target\ddclient-noicon.exe ddclient
+
+rem win32::exe no longer works
+rem %PERL% -e "use Win32::Exe; $exe = Win32::Exe->new('..\..\..\target\ddclient.exe'); $exe->set_single_group_icon('..\resources\ddclient32.ico'); $exe->write;"
+
+rem ResourceHacker.exe -open ..\..\..\target\ddclient.exe -save ddclient.exe -action modify -res ..\resources\ddclient32.ico -mask ICONGROUP,WINEXE,1033
+ResourceHacker.exe -open ..\..\..\target\ddclient-noicon.exe -save ..\..\..\target\ddclient.exe -action modify -res ..\resources\ddclient32.ico -mask ICONGROUP,WINEXE,1033
+del ..\..\..\target\ddclient-noicon.exe
+verpatch ..\..\..\target\ddclient.exe %DDCLIENT_VERSION% /fn /s company "randomnoun" /s description "A Dynamic DNS client" /s copyright "(c) 2013-2021 randomnoun Creative Commons Attribution 3.0 Unported License" /s product ddclient /pv %DDCLIENT_VERSION%
 
 echo Compiling ddclient-noconsole.exe
-%PERL% %PERL%\..\..\site\bin\pp --gui -o ..\..\..\target\ddclient-noconsole.exe ddclient
-%PERL% -e "use Win32::Exe; $exe = Win32::Exe->new('..\..\..\target\ddclient-noconsole.exe'); $exe->set_single_group_icon('..\resources\ddclient32.ico'); $exe->write;"
-verpatch ..\..\..\target\ddclient-noconsole.exe %DDCLIENT_VERSION% /fn /s company "randomnoun" /s description "A Dynamic DNS client" /s copyright "(c) 2013-2014 randomnoun Creative Commons Attribution 3.0 Unported License" /s product ddclient /pv %DDCLIENT_VERSION%
+%PERL% %PERL%\..\..\site\bin\pp --gui -o ..\..\..\target\ddclient-noconsole-noicon.exe ddclient
+rem %PERL% -e "use Win32::Exe; $exe = Win32::Exe->new('..\..\..\target\ddclient-noconsole.exe'); $exe->set_single_group_icon('..\resources\ddclient32.ico'); $exe->write;"
+rem ResourceHacker.exe -open ..\..\..\target\ddclient-noconsole.exe -save ddclient-noconsole2.exe -action modify -res ..\resources\ddclient32.ico -mask ICONGROUP,WINEXE,1033
+ResourceHacker.exe -open ..\..\..\target\ddclient-noconsole-noicon.exe -save ..\..\..\target\ddclient-noconsole.exe -action modify -res ..\resources\ddclient32.ico -mask ICONGROUP,WINEXE,1033
+del ..\..\..\target\ddclient-noconsole-noicon.exe
+verpatch ..\..\..\target\ddclient-noconsole.exe %DDCLIENT_VERSION% /fn /s company "randomnoun" /s description "A Dynamic DNS client" /s copyright "(c) 2013-2021 randomnoun Creative Commons Attribution 3.0 Unported License" /s product ddclient /pv %DDCLIENT_VERSION%
 
 exit 0
